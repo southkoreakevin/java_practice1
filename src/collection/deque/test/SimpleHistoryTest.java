799e2e1b80007f65d55d21f3e4922a19c8de210a
kevin <dongohkang@gangdong-oui-MacBookPro.local>
2025-08-04 23:50:32 +0900

    2025_08_04

---
+++ src/collection/deque/test/SimpleHistoryTest.java
@@ -0,0 +1,4 @@
+package collection.deque.test;
+
+public class SimpleHistoryTest {
+}
---
+++ src/collection/deque/test/stack/BrowserHistoryTest.java
@@ -0,0 +1,19 @@
+package collection.deque.test.stack;
+
+public class BrowserHistory {
+
+    public static void main(String[] args) {
+        BrowserHistory browser = new BrowserHistory();
+
+        browser.visitPage("youtube.com");
+        browser.visitPage("google.com");
+        browser.visitPage("facebook.com");
+
+        String currentPage1 = browser.goBack();
+        System.out.println("currentPage1 = " + currentPage1);
+
+        String currentPage2 = browser.goBack();
+        System.out.println("currentPage2 = " + currentPage2);
+    }
+
+}
